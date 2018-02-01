e03047f030192bbbb011a5ed199c97ba184d137b
Roman Bogdanov <bogdanovroman1221@gmail.com>
2018-02-01 21:15:17 +0200

    change ArrayList to HashMap for storing tasks; deleteTask() method

--- src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/Application.java
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/Application.java
@@ -5,19 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
 
-    public static ArrayList<Task> toDoList = new ArrayList<>();
+    public static HashMap<Long, Task> toDoMap = new HashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
         Task task1 = new Task("bring water");
         Task task2 = new Task("finish the test task", LocalDate.of(2018, Month.FEBRUARY, 5));
-        toDoList.add(task1);
-        toDoList.add(task2);
+        toDoMap.put(task1.getId(), task1);
+        toDoMap.put(task2.getId(), task2);
     }
 }
--- src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/TaskService.java
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/TaskService.java
@@ -1,25 +1,28 @@
 package com.miratech.test_task_todo_list_bogdanov_roman;
 
-import org.apache.tomcat.jni.Local;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/rest/task")
 public class TaskService {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ArrayList<Task> getAllTasks() {
-        return Application.toDoList;
+    public HashMap<Long, Task> getAllTasks() {
+        return Application.toDoMap;
     }
 
     @RequestMapping(value = "/add"/*, method = RequestMethod.POST*/)    //do not specify a particular REST request(POST) to use it in address bar
     public Task addTask(@RequestParam(value = "title", required = false, defaultValue = "default title") String title) {
         Task task = new Task(title);
-        Application.toDoList.add(task);
+        Application.toDoMap.put(task.getId(), task);
         return task;
+    }
+
+    @RequestMapping(value = "/delete/{id}"/*, method = RequestMethod.DELETE*/)  //do not specify a particular REST request(DELETE) to use it in address bar
+    public Task deleteTask(@PathVariable long id){
+        return Application.toDoMap.remove(id);
     }
 
 
