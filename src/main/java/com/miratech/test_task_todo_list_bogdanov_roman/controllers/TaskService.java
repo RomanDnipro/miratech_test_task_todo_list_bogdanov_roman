14f259de32d23c4c74e7138feae5ea6d53d2767a
Roman Bogdanov <bogdanovroman1221@gmail.com>
2018-02-03 15:47:55 +0200

    finished updating TaskService.java for work with db

--- src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/controllers/TaskService.java
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/controllers/TaskService.java
@@ -1,6 +1,5 @@
 package com.miratech.test_task_todo_list_bogdanov_roman.controllers;
 
-import com.miratech.test_task_todo_list_bogdanov_roman.Application;
 import com.miratech.test_task_todo_list_bogdanov_roman.models.Task;
 import com.miratech.test_task_todo_list_bogdanov_roman.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.xml.ws.Response;
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -55,43 +52,8 @@ public class TaskService {
         return new ResponseEntity<>(updatedTask, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/done/{id}"/*, method = RequestMethod.PUT*/)
-    //do not specify a particular REST request(PUT) to use it in browser address bar
-    public Task setDone(@PathVariable long id) throws Exception {
-
-        return changeDone(id, true);
-    }
-
-    @RequestMapping(value = "/notDone/{id}"/*, method = RequestMethod.PUT*/)
-    //do not specify a particular REST request(PUT) to use it in browser address bar
-    public Task setNotDone(@PathVariable long id) throws Exception {
-        return changeDone(id, false);
-    }
-
-    @RequestMapping(value = "/switchDone/{id}"/*, method = RequestMethod.PUT*/)
-    //do not specify a particular REST request(PUT) to use it in browser address bar
-    public Task switchDone(@PathVariable long id) throws Exception {
-        if (Application.toDoMap.containsKey(id)) {
-            Task task = Application.toDoMap.get(id);
-            task.setDone(!task.isDone());
-            return task;
-        } else {
-            throw new Exception("Task " + id + " does not exists");
-        }
-    }
-
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable long id) {
         taskRepository.delete(id);
-    }
-
-    private Task changeDone(long id, boolean isDone) throws Exception {
-        if (Application.toDoMap.containsKey(id)) {
-            Task task = Application.toDoMap.get(id);
-            task.setDone(isDone);
-            return task;
-        } else {
-            throw new Exception("Task " + id + " does not exists");
-        }
     }
 }
