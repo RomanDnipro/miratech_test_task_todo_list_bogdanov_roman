691a994097a62fbe6d6ecff25cc87e649a8ea9c7
Roman Bogdanov <bogdanovroman1221@gmail.com>
2018-02-03 15:07:22 +0200

    change getAllTasks() to work with db

--- src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/controllers/TaskService.java
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/controllers/TaskService.java
@@ -1,17 +1,27 @@
-package com.miratech.test_task_todo_list_bogdanov_roman;
+package com.miratech.test_task_todo_list_bogdanov_roman.controllers;
 
+import com.miratech.test_task_todo_list_bogdanov_roman.Application;
 import com.miratech.test_task_todo_list_bogdanov_roman.models.Task;
+import com.miratech.test_task_todo_list_bogdanov_roman.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/rest/task")
+@RequestMapping("/api")
+@CrossOrigin("*")
 public class TaskService {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public HashMap<Long, Task> getAllTasks() {
-        return Application.toDoMap;
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/")
+    public List<Task> getAllTasks() {
+        Sort sortByCreatedAtDesc = new Sort(Sort.Direction.DESC, "createdAT");
+        return taskRepository.findAll(sortByCreatedAtDesc);
     }
 
     @RequestMapping(value = "/add"/*, method = RequestMethod.POST*/)
