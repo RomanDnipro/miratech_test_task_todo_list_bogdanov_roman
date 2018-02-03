a02abb6041ab4939c28fb154370df94f70cd9cc3
Roman Bogdanov <bogdanovroman1221@gmail.com>
2018-02-03 14:32:11 +0200

    added interface TaskRepository

---
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/repositories/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.miratech.test_task_todo_list_bogdanov_roman.repositories;
+
+import com.miratech.test_task_todo_list_bogdanov_roman.models.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository {
+
+}
