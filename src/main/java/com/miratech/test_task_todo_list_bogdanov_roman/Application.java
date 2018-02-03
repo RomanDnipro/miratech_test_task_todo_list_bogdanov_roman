083c8c0bb476e12c4d99636c874ab4e37cd8e2cd
Roman Bogdanov <bogdanovroman1221@gmail.com>
2018-02-03 23:26:54 +0200

    server id done

--- src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/Application.java
+++ src/main/java/com/miratech/test_task_todo_list_bogdanov_roman/Application.java
@@ -11,14 +11,14 @@ import java.util.HashMap;
 @SpringBootApplication
 public class Application {
 
-    public static HashMap<Long, Task> toDoMap = new HashMap<>();
+//    public static HashMap<Long, Task> toDoMap = new HashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        Task task1 = new Task("test1 bring water");
-        Task task2 = new Task("test2 finish the test task", LocalDate.of(2018, Month.FEBRUARY, 5));
-        toDoMap.put(task1.getId(), task1);
-        toDoMap.put(task2.getId(), task2);
+//        Task task1 = new Task("test1 bring water");
+//        Task task2 = new Task("test2 finish the test task", LocalDate.of(2018, Month.FEBRUARY, 5));
+//        toDoMap.put(task1.getId(), task1);
+//        toDoMap.put(task2.getId(), task2);
     }
 }
